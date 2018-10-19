befe403984e0fe27bbef4107181db5cb35629597
Unknown <JonathanILevi@gmail.com>
2018-10-19 20:35:18 +0000

    New Galactic Down Protocol

--- code/src/galactic_msg_/down_.d
+++ code/src/galactic_msg_/down_.d
@@ -6,7 +6,10 @@ import cst_;
 import loose_.net_msg_;
 
 enum MsgType {
+	add	,
+	remove	,
 	update	,
+	moveAll	,
 }
 
 struct UnknownMsg {
@@ -20,7 +23,48 @@ struct UnknownMsg {
 }
 
 
-
+class AddMsg {
+	import galactic_msg_.entity_;
+	static AddMsg opCall() {
+		return new AddMsg;
+	}
+	static AddMsg opCall(UnknownMsg msg) {
+		return AddMsg(msg.msgData);
+	}
+	static AddMsg opCall(const(ubyte)[] msgData) {
+		return msgData.decodeNetMsg!AddMsg(MsgType.add);
+	}
+	
+	@Net ushort	id	;
+	@Net float[2]	pos	;
+	@Net float	ori	;
+	
+	@property
+	const(ubyte)[] msgData() {
+		return this.encodeNetMsg(MsgType.add);
+	}
+	alias msgData this;
+}
+class RemoveMsg {
+	import galactic_msg_.entity_;
+	static RemoveMsg opCall() {
+		return new RemoveMsg;
+	}
+	static RemoveMsg opCall(UnknownMsg msg) {
+		return RemoveMsg(msg.msgData);
+	}
+	static RemoveMsg opCall(const(ubyte)[] msgData) {
+		return msgData.decodeNetMsg!RemoveMsg(MsgType.remove);
+	}
+	
+	@Net ushort	id	;
+	
+	@property
+	const(ubyte)[] msgData() {
+		return this.encodeNetMsg(MsgType.remove);
+	}
+	alias msgData this;
+}
 class UpdateMsg {
 	import galactic_msg_.entity_;
 	static UpdateMsg opCall() {
@@ -32,7 +76,10 @@ class UpdateMsg {
 	static UpdateMsg opCall(const(ubyte)[] msgData) {
 		return msgData.decodeNetMsg!UpdateMsg(MsgType.update);
 	}
-	@Net Entity[] entities;
+	
+	@Net ushort	id	;
+	@Net float[2]	pos	;
+	@Net float	ori	;
 	
 	@property
 	const(ubyte)[] msgData() {
@@ -40,6 +87,28 @@ class UpdateMsg {
 	}
 	alias msgData this;
 }
+class MoveAllMsg {
+	import galactic_msg_.entity_;
+	static MoveAllMsg opCall() {
+		return new UpdateMsg;
+	}
+	static MoveAllMsg opCall(UnknownMsg msg) {
+		return MoveAllMsg(msg.msgData);
+	}
+	static MoveAllMsg opCall(const(ubyte)[] msgData) {
+		return msgData.decodeNetMsg!MoveAllMsg(MsgType.moveAll);
+	}
+	
+	@Net float[2]	pos	;
+	@Net float	ori	;
+	
+	@property
+	const(ubyte)[] msgData() {
+		return this.encodeNetMsg(MsgType.moveAll);
+	}
+	alias msgData this;
+}
+
 
 
 

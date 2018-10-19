b851ca24b94274cdf577605ff6bd3f51c59fdd13
Unknown <JonathanILevi@gmail.com>
2018-10-19 23:07:51 +0000

    Reordered entity id in ship.

--- code/src_ship/ship_/world_/entity_.d
+++ code/src_ship/ship_/world_/entity_.d
@@ -6,17 +6,16 @@ import cst_;
 
 class Entity {
 	this() {}
-	this(	float[2]	pos	,
-		float	ori	,
-		ushort	id	,) {
+	this(	ushort	id	,
+		float[2]	pos	,
+		float	ori	,) {
 		
 		this.pos	= pos	;
 		this.ori	= ori	;
-		this.id	= id	;
 	}
+	ushort	id	;
 	float[2]	pos	;
 	float	ori	;
-	ushort	id	;
 	bool	updated	; // Used when galactic sends update (in ship_.galactic_.galactic_mgr_.d)
 }
 

27faff39d50d467bd54e1adb0983185ee61a06b9
Unknown <JonathanILevi@gmail.com>
2018-10-23 14:50:46 +0000

    Removed logging

--- code/src/galactic_msg_/msg_template_.d
+++ code/src/galactic_msg_/msg_template_.d
@@ -14,16 +14,12 @@ mixin template  MsgTemplate(){
 	}
 	static Msg opCall(const(ubyte)[] msgData) {
 		assert(msgData[0] == msgData.length);
-		msgData.log;
-		type.log;
 		return msgData[2..$].deserialize!(Msg, Endian.littleEndian, ubyte);
 	}
 	
 	@property
 	const(ubyte)[] msgData() {
 		auto data = this.serialize!(Endian.littleEndian, ubyte);
-		type.log;
-		data.log;
 		data = [(data.length+2).cst!ubyte, type.cst!ubyte]~data;
 		assert(data.length == data[0]);
 		return data;
